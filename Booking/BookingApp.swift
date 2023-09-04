43c4e64cbceea25731e56ee33df4fa2c59ae2bac
Leonid Latyshev <forlemonster@gmail.com>
2023-09-04 15:35:08 +1000

    Initial Commit

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Яна Латышева on 04.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
